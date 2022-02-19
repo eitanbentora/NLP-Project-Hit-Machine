7cd0cbf46b740f25d965c13efaa80a0d5203782e
eitan.bentora <eitan.bentora@gmail.com>
2022-02-19 09:32:32 +0200

    just checking

--- Code/DataCleaning/predict_lang_and_split_chords_and_lyrics.py
+++ Code/DataCleaning/predict_lang_and_split_chords_and_lyrics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import data_cleaning as data_cleaning
 from tqdm import tqdm
-tqdm.pandas()
+tqdm.pandas() #bedek
 
 chords_df = pd.read_pickle('/home/student/Desktop/Project/Data/chords.pkl')
 print("spliting")
