b258f88d34ecc479e69ae5c175efb941a9e9369f
eitan.bentora <eitan.bentora@gmail.com>
2022-02-25 17:41:18 +0200

    training process ready

--- Code/DataCleaning/data_cleaning.py
+++ Code/DataCleaning/data_cleaning.py
@@ -12,32 +12,61 @@ def predict_lang(text):
     return lang
 
 def is_chord(word):
-    chord_regex = r"^\(?([ABCDEFG])([#b]?)(m?)(\(?[245679]?\)?)(\-?)(/?)((dim)|(sus)|(maj)|(aug)|)(\+?)(add)?(\(?[245679]?\)?)M?(\*?)((/[ABCDEFG][#b]?)?)(\(hold\))?\)?$"
+    chord_regex = r"^\(?([ABCDEFG])([#b]?)(m?)-?(\(?[245679]?\)?)(\-?)(/?)((dim)|(sus)|(maj)|(aug)|)(\+?)(add)?(\(?([245679]|11|13)?\)?)M?(\*?)((/[ABCDEFG][#b]?)?)(\(hold\))?\)?$"
     return bool(re.match(chord_regex, word)) and word != ''
 
+
 def is_chord_line(line):
-    special_words= ['Intro:', 'X2' , 'x2', 'Intro', 'Solo', '2X)(', '(2X)', '(2x)', 'Interlude:', '(', ')', 'break', '(BREAK)']
+    special_words= ['intro', 'x2', 'solo', 'interlude', 'break', '%', 'bridge', 'introdução']
+    
+    if line.lower().startswith('bridge:'):
+        return True
+    
     if len(line) == 0:
         return False
-    is_chord_line=True
-    for word in re.split(r'[ +]',line):
-        if not is_chord(word) and word not in special_words and word != '':
-            return False
-    return True
+    
+    chord_count, word_count = 0, 0
+    
+    for word in re.split(r' |,|\||:|\(|\)| -| -|\t|\n',line):
+        if word != '':
+            word_count += 1
+        if is_chord(word) or word.lower() in special_words:
+            chord_count += 1
+    if word_count == 0:
+        return False
+    if chord_count/word_count >= 1/2:
+        return True
+    return False
 
 def is_tab_line(line):
-    tab_regex = r"^([ABCDEFGabcde]?)([#b]?)(\|*)([\d\-/\^><\|]+)( *)(\|*) *$"
+    if 'hide this tab' in line.lower():
+        return True
+    if line.count('-') > 4:
+        return True
+    tab_regex = r"^ *([ABCDEFGabcde]?)([#b]?)(\|*)([\d\-\^><\|]+)( *)(\|*) *$"
     return bool(re.match(tab_regex, line)) and len(line) > 0
+    
 
+def remove_line(line):
+    if re.match(' *^_+ *$', line):
+        return True
+    remove = ['capo on']
+    for word in re.split(r'[ +]',line):
+        if word.lower() in remove:
+            return True
+    return False
+    
 
 def split_chords_lyrics_and_tabs(song):
     lines = [x for x in song.split('\r\n')]
     chords, lyrics, tabs = {}, {}, {}
-    for i, line in enumerate(lines):      
-        if is_tab_line(line):
-            tabs[i] = line
+    for i, line in enumerate(lines):  
+        if remove_line(line):
+            continue
         elif is_chord_line(line):
             chords[i] = line
+        elif is_tab_line(line):
+            tabs[i] = line
         else:
             lyrics[i] = line
     return chords, lyrics, tabs
